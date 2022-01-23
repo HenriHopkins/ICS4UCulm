1c7f4dbe37d0345195fc4c287ac7f5f3482d840e
HenriHopkins <97988869+HenriHopkins@users.noreply.github.com>
2022-01-23 18:56:17 -0500

    Create PlayGame.java

---
+++ PlayGame.java
@@ -0,0 +1,49 @@
+import java.util.Random;
+import java.util.Scanner;
+public class Main //"main" will be changed upon integration
+{
+	public static void main(String[] args) {
+	int c = 0;
+	playGame(c);
+	}
+	
+	public static void playGame(c){
+	    int players = 1; //obviously integrate diff variable, players is placeholder
+	    Scanner input = new Scanner(System.in);
+	  //  int co = 0; //may use this for recursion but not sure yet
+	    if (players ==1) {
+	        String ans = "";
+	       	Random r = new Random();
+		int num =r.nextInt(q.size())+1; //may have to adjust random based on looping
+System.out.println(q.get(num));
+System.out.println(op.get(num));
+//timer.start();
+ans  = input.nextLine();
+//if timer>time limit decided by court, timer.cancel(); timer.purge(); do recursion
+//maybe implement while loop with c condition to prevent infinite loop and whatnot
+if (ans==answers.get(num)) {System.out.println("correct!") scoreArr.get(0)++;};
+    else {System.out.println("Incorrect!"); }//integrate recursion here
+}
+ 
+if (players>1) { //couldnt this just be an else lol
+ int player;
+ if (c=0) {player=c;}
+ else if (c<scoreArr.size() {c++;player=c;}
+ else if (c==scoreArr.size() {c=0; player=c;}
+	    Scanner input = new Scanner(System.in);
+	  //  int co = 0; //may use this for recursion but not sure yet
+	    if (players ==1) {
+	        String ans = "";
+	       	Random r = new Random();
+		int num =r.nextInt(q.size())+1; //may have to adjust random based on looping
+System.out.println(q.get(num));
+System.out.println(op.get(num));
+//timer.start();
+ans  = input.nextLine();
+//if timer>time limit decided by court, timer.cancel(); timer.purge(); do recursion
+//maybe implement while loop with c condition to prevent infinite loop and whatnot
+if (ans==answers.get(num)) {System.out.println("correct!") scoreArr.get(c)++;};
+    else {System.out.println("Incorrect!"); }//integrate recursion here
+	    }
+	}
+}
